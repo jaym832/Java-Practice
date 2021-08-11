1f8ca6e4034e8ffb6487549d2d2762c03a675092
Javier Marin <jjmarin777@gmail.com>
2021-08-11 14:54:16 -0500

    first commit

---
+++ src/main.java
@@ -0,0 +1,25 @@
+public class main {
+    public static void main (String[] args){
+
+
+
+       int totalNums= addingNums(10,20,30);
+
+       int anotherNum=200;
+
+        System.out.println(totalNums);
+
+
+
+
+
+
+}
+
+public static int addingNums(int num1,int num2,int num3){
+        int myTotal=num1+num2+num3;
+        return myTotal;
+}
+
+
+}
